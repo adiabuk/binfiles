fbf0582b5a69ae815a14d861f2c6d93fc74376f7
Amro Diab <adiab@hotmail.co.uk>
2017-12-03 14:15:02 +0000

    get public IP of EC2 instance

--- publicip.sh
+++ publicip.sh
@@ -4,6 +4,12 @@
 
 set -eu
 set -o pipefail
-result=`wget -qO- http://ipecho.net/plain` || echo "No Address"
+aws='http://instance-data/latest/meta-data/public-ipv4'
+if  host instance-data > /dev/null; then
+  result=$(wget -q0- http://instance-data/latest/meta-data/public-ipv4);
+else
+  result=$(wget -qO- http://ipecho.net/plain) || echo "No Address"
+fi
+
 echo $result | grep -qi html && echo "No Address" && exit 1
 echo $result && echo
