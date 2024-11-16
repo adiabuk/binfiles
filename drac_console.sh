6c955ef690890821d0c77a0afa18fca7e67ed071
Amro Diab <adiab@hotmail.co.uk>
2024-11-16 16:33:57 +0000

    drac script fixes

--- drac_console.sh
+++ drac_console.sh
@@ -1,6 +1,6 @@
 read -p "hostname: " hostname
-echo -n Password:
+echo Password:
 read -s password
 #echo
 #echo $hostname $password
-/usr/bin/java -Djava.security.properties=idrac.java.security -cp avctKVM2.jar com.avocent.idrac.kvm.Main ip=${hostname}-idrac kmport=5900 vport=5900 user=root passwd=$password apcp=1 version=2 vmprivilege=true "helpurl=https://${hostname}-idrac/help/contents.html"
+/usr/bin/java -Djava.security.properties=idrac.java.security -cp avctKVM2.jar com.avocent.idrac.kvm.Main ip=${hostname}-idrac kmport=5900 vport=5900 user=root passwd=$password apcp=1 version=2 vmprivilege=true "helpurl=https://${hostname}/help/contents.html"
